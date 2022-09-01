5d6753560bb2899f57c4b10f9095f7c5ed1a6aae
moog.kwon <diligentaf@gmail.com>
2022-09-02 08:42:16 +0900

    added some sample tags

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,35 @@
 <template>
-  Badass Todo
+  <div class="badass-todo">
+    <div class="title has-text-centered">Badass Todo</div>
+
+    <div class="field is-grouped mb-5">
+      <div class="control is-expanded">
+        <input class="input" type="text" placeholder="Add a todo" />
+      </div>
+      <div class="control">
+        <a class="button is-info"> Add </a>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros.
+          Donec id elit non mi porta gravida at eget metus. Cum sociis natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Cras mattis consectetur purus sit amet fermentum.
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
+
+<style>
+@import "bulma/css/bulma.min.css";
+.badass-todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
